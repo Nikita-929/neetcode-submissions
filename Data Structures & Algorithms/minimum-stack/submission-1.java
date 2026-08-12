ab64df0c278f85a841f63e0458eb0675f10a74e7
Nimbus <mahatonikita929@gmail.com>
2026-08-12 20:49:25 +0530

    Add: minimum-stack - submission-1

---
+++ Data Structures & Algorithms/minimum-stack/submission-1.java
@@ -0,0 +1,27 @@
+class MinStack {
+    private Stack<int[]> st;
+    
+    public MinStack() {
+        st = new Stack<>();
+    }
+    
+    public void push(int val) {
+        int min = st.isEmpty()? val:Math.min(val, st.peek()[1]);
+        
+        st.push(new int[]{val,min});
+        
+    }
+    
+    public void pop() {
+        
+        st.pop();
+    }
+    
+    public int top() {
+       return st.peek()[0];
+    }
+    
+    public int getMin() {
+      return  st.peek()[1];
+    }
+}
