ae5fb037da7408a388c2dcc361fbbbe0397600b7
Nimbus <mahatonikita929@gmail.com>
2026-08-12 13:31:38 +0530

    Add: is-palindrome - submission-1

---
+++ Data Structures & Algorithms/is-palindrome/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        char []arr = s.toCharArray();
+         int l=0;
+         int r =s.length()-1;
+         while(l<r){
+            while(l< r && !Character.isLetterOrDigit(arr[l])){
+                l++;
+            }
+            while(l<r && !Character.isLetterOrDigit(arr[r])){
+                r--;
+            }
+            if(Character.toLowerCase(arr[l])!=Character.toLowerCase(arr[r])){
+                return false;
+            }
+            l++;
+            r--;
+         }
+         return true;
+    }
+}
